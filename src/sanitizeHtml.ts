aead59f3e523c0fffc9a1a07e8c1d55cd69c9f7b
Luc Gagan <lucgagan@gmail.com>
2023-11-12 12:10:37 -0600

    feat: add select, textarea and img to the list of allowed tags

--- src/sanitizeHtml.ts
+++ src/sanitizeHtml.ts
@@ -1,11 +1,19 @@
 import sanitize from "sanitize-html";
 
+/**
+ * The reason for sanitization is because we do not need all HTML tags to be present in the prompt.
+ * For example, we do not need <style> or <script> tags to be present in the prompt.
+ * In my experience, reducing HTML only to basic tags produces faster and more reliable prompts.
+ */
 export const sanitizeHtml = (subject: string) => {
   return sanitize(subject, {
     allowedTags: sanitize.defaults.allowedTags.concat([
-      "form",
-      "input",
       "button",
+      "form",
+      "img",
+      "input",
+      "select",
+      "textarea",
     ]),
     allowedAttributes: false,
   });

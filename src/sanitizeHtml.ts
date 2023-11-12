868534e18f8c24bdea084ff29530b2729dc7fae8
Luc Gagan <lucgagan@gmail.com>
2023-11-12 12:29:13 -0600

    docs: document HTML sanitization

--- src/sanitizeHtml.ts
+++ src/sanitizeHtml.ts
@@ -1,9 +1,15 @@
 import sanitize from "sanitize-html";
 
 /**
- * The reason for sanitization is because we do not need all HTML tags to be present in the prompt.
- * For example, we do not need <style> or <script> tags to be present in the prompt.
+ * The reason for sanitization is because OpenAI does not need all of the HTML tags
+ * to know how to interpret the website, e.g. it will not make a difference to AI if
+ * we include or exclude <script> tags as they do not impact the already rendered DOM.
+ * 
  * In my experience, reducing HTML only to basic tags produces faster and more reliable prompts.
+ * 
+ * Note that the output of this function is designed to interpret only the HTML tags.
+ * For instructions that rely on visual cues (e.g. "click red button") we intend to
+ * combine HTML with screenshots in the future versions of this library.
  */
 export const sanitizeHtml = (subject: string) => {
   return sanitize(subject, {

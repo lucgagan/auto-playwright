bf4590437ce5e31e2464cc8da56baaa93daa27a2
Luc Gagan <lucgagan@gmail.com>
2023-11-12 12:20:13 -0600

    docs: document that instructions are derived from functions

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,5 +1,11 @@
 import { TaskMessage } from "./types";
 
+/**
+ * The prompt itself is very simple because the vast majority of the logic is derived from
+ * the instructions contained in the parameter and function descriptions provided to `openai.beta.chat.completions`.
+ * @see https://www.npmjs.com/package/openai#automated-function-calls
+ * @see https://openai.com/blog/function-calling-and-other-api-updates
+ */
 export const prompt = (message: TaskMessage) => {
   return `This is your task:
 

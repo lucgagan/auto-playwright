56e8c47344c49dcfdd9c65dbe162a1cb81c9d938
Raj <rajeshdavid.b@gmail.com>
2024-03-31 20:53:41 +0200

    use run tools instead of run functions

--- src/completeTask.ts
+++ src/completeTask.ts
@@ -1,9 +1,9 @@
-import OpenAI from "openai";
-import { type Page, TaskMessage, TaskResult } from "./types";
-import { prompt } from "./prompt";
-import { createActions } from "./createActions";
+import OpenAI from 'openai';
+import { type Page, TaskMessage, TaskResult } from './types';
+import { prompt } from './prompt';
+import { createActions } from './createActions';
 
-const defaultDebug = process.env.AUTO_PLAYWRIGHT_DEBUG === "true";
+const defaultDebug = process.env.AUTO_PLAYWRIGHT_DEBUG === 'true';
 
 export const completeTask = async (
   page: Page,
@@ -19,19 +19,22 @@ export const completeTask = async (
   const debug = task.options?.debug ?? defaultDebug;
 
   const runner = openai.beta.chat.completions
-    .runFunctions({
-      model: task.options?.model ?? "gpt-4-1106-preview",
-      messages: [{ role: "user", content: prompt(task) }],
-      functions: Object.values(actions),
+    .runTools({
+      model: task.options?.model ?? 'gpt-4-1106-preview',
+      messages: [{ role: 'user', content: prompt(task) }],
+      tools: Object.values(actions).map((action) => ({
+        type: 'function',
+        function: action,
+      })),
     })
-    .on("message", (message) => {
+    .on('message', (message) => {
       if (debug) {
-        console.log("> message", message);
+        console.log('> message', message);
       }
 
       if (
-        message.role === "assistant" &&
-        message.function_call?.name.startsWith("result")
+        message.role === 'assistant' &&
+        message.function_call?.name.startsWith('result')
       ) {
         lastFunctionResult = JSON.parse(message.function_call.arguments);
       }
@@ -40,15 +43,15 @@ export const completeTask = async (
   const finalContent = await runner.finalContent();
 
   if (debug) {
-    console.log("> finalContent", finalContent);
+    console.log('> finalContent', finalContent);
   }
 
   if (!lastFunctionResult) {
-    throw new Error("Expected to have result");
+    throw new Error('Expected to have result');
   }
 
   if (debug) {
-    console.log("> lastFunctionResult", lastFunctionResult);
+    console.log('> lastFunctionResult', lastFunctionResult);
   }
 
   return lastFunctionResult;
